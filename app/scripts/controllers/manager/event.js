a5b9fb9ff8c739cb17a1d589bc08c3c8d4c198df
Clay Diffrient <clay.diffrient@gmail.com>
2013-07-28 00:23:05 -0600

    Inital work on add slot modal

--- app/scripts/controllers/manager/event.js
+++ app/scripts/controllers/manager/event.js
@@ -2,6 +2,20 @@
 
 angular.module('signupSystemApp')
   .controller('ManagerEventCtrl', function ($scope, $routeParams, Event) {
+
+      $scope.showName = false;
+
       $scope.id = $routeParams.id;
-      $scope.event = Event.get({id: $scope.id});
+      $scope.event = Event.get({id: $scope.id}, function (event)
+         {
+            if (event.slots.length > 0)
+            {
+               event.slots.forEach(function (slot){
+                  if (slot.name)
+                  {
+                     $scope.showName = true;
+                  }
+               });
+            }
+         });
   });
